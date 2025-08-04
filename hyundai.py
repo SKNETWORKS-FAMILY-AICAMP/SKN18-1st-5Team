6b0b283c8ced170d037eac45a965a65e40d3e447
sonjuyeong-00 <sonju9638@gmail.com>
2025-08-04 18:34:21 +0900

    수정2

--- hyundai.py
+++ hyundai.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 
 ###########함수 라이브러리###########
 from common.utils import hyundai_load, Annual_total_load, selected_year
